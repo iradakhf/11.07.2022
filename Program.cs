ad7f8357e180525eeaa1ac14fccbd0359990e1c9
iradakhf <iradakhf@code.edu.az>
2022-07-11 08:11:02 +0400

    add all

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace ConsoleApp28
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            string[,] smileys = new string[3, 2] { { ":)", ":D" }, { ":(", ":D" }, { ":)", ":<" } };
+
+
+            Matrix matrix = new Matrix();
+            int count = matrix.CountSmileyFace(smileys);
+
+            Console.WriteLine(count);
+
+        }
+        public class Matrix
+        {
+            public int CountSmileyFace(string[,] smileys)
+            {
+                int count = 0;
+                for (int i = 0; i < smileys.GetLength(0); i++)
+                {
+                    for (int j = 0; j < smileys.GetLength(1); j++)
+                    {
+                        if (smileys[i, j] == ":)" || smileys[i, j] == ":D")
+                        {
+                            count++;
+                        }
+                    }
+                }
+                return count;
+
+            }
+        }
+    }
+}
